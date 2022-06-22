49f7559edda6b5d212943336e1ea1c74a6bead16
Emma Secrest <esecrest@dminc.com>
2022-06-22 10:25:16 -0400

    delete now working

--- front-end/index.ts
+++ front-end/index.ts
@@ -6,9 +6,6 @@ type Todo = {
 };
 
 
-
-
-
 async function getAllTodos(): Promise<Todo[]> {
   const response = await fetch("http://localhost:3000/todos");
   return response.json();
@@ -29,24 +26,73 @@ async function createTodo(): Promise<Todo> {
     return response.json();
 }
 
+async function deleteTodo(nodeID:string):Promise<Todo>{
+  const response = await fetch(`http://localhost:3000/todo/${nodeID}`,{
+    method: "DELETE",
+    headers:{
+      "Content-Type": "application/json"
+    }})
+    return response.json();
+  }
+
+
 const addButton = document.getElementById("add");
 if(addButton === null){
   throw new Error("Something went Wrong")
 }
 addButton.onclick =  () =>{
   createTodo()
+  
   window.location.reload()
   }
 
-(async () => {
+
+
+  (async () => {
     const list = document.getElementById("todo-list");
     if (!list) throw new Error("Element not found");
     const todos = await getAllTodos();
+    
+    
+    
     todos.forEach((todo) => {
       const listItem = document.createElement("li");
       listItem.classList.add("list-group-item");
-      listItem.innerHTML = `${todo.value} <button class='btn btn-danger float-end'>Delete</button>`;
+      // const buttonId = Date.now();
+      // const buttonIdString = buttonId.toString();
+      listItem.id = todo.id
+     
+      const deleteButton = document.createElement('button')
+      deleteButton.classList.value ='btn btn-danger float-end'
+      // deleteButton.id = buttonIdString
+      deleteButton.textContent = 'Delete'
+      
+      deleteButton.onclick  = () => {
+        // console.log(buttonIdString)
+        deleteTodo(todo.id);
+        window.location.reload()
+      }
+      
+      listItem.innerHTML = `${todo.value}`;
+      listItem.appendChild(deleteButton)
+      // console.log(buttonId)
       list.appendChild(listItem);
     });
   })();
+
+  // console.log(buttonIdString)
+//  const deleteButton = document.getElementById(buttonIdString) 
+// //  console.log( "delete button:  "+ deleteButton)
+//  if(!deleteButton){
+//   // console.log(buttonIdString)
+//   throw new Error('Delete button is null')
+//  }
+
+//  deleteButton.onclick  = () => {
+//   deleteTodo(buttonIdString);
+//   window.location.reload()
+// }
+
+
+
   
